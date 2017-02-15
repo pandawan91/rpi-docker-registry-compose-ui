943221f5a2176992d23d5bef27a9c9684c541f9e
Vuong <vuongngo@outlook.de>
2017-02-15 10:39:05 +0100

    update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json({
   type: 'application/vnd.docker.distribution.events.v1+json'
 }));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080/");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
